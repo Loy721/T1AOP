569a38b556b669ee57b833560e7d58b68e0c6bf5
Долгих Данила Юрьевич <danil.dolgikh@simbirsoft.com>
2024-07-30 23:57:14 +0000

    git init

---
+++ src/main/java/com/loy/t1aop/T1aopApplication.java
@@ -0,0 +1,13 @@
+package com.loy.t1aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1aopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T1aopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/loy/t1aop/T1aopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loy.t1aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1aopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
